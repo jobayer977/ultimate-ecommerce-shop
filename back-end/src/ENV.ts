3b94f7a0d37117634dfb410d64d25b7fdb7414b6
Jobayer Hossain <Jobayerhossain977@gmail.com>
2021-06-02 03:45:17 +0600

    master: Updated master: Updated

--- back-end/src/ENV.ts
+++ back-end/src/ENV.ts
@@ -1,33 +1,31 @@
-import * as config from "config"
-
 export const ENV = {
-	port: config.server.port,
+	port: 3000,
 
-	API_PREFIX: config.docs.API_PREFIX,
-	API_TITLE: config.docs.API_TITLE,
-	API_DESC: config.docs.API_DESC,
-	API_VERSION: config.docs.API_VERSION,
-	API_DOCS_URL: config.docs.API_PREFIX + "docs",
+	API_PREFIX: "/api/v1/",
+	API_TITLE: "iShop",
+	API_DESC: "iShop Api V1",
+	API_VERSION: 1.0,
+	API_DOCS_URL: "/api/v1/" + "docs",
 
-	jwtSecret: config.JWTSecret,
-	slatRound: config.saltRound,
+	jwtSecret: "topSecret51",
+	slatRound: 10,
 
-	MAIL_HOST: config.MAIL_HOST,
-	MAIL_PORT: config.MAIL_PORT,
-	MAIL_SECURE: config.MAIL_SECURE,
-	MAIL_USER: config.MAIL_USER,
-	MAIL_PASSWORD: config.MAIL_PASSWORD,
-	MAIL_FROM: config.MAIL_FROM,
+	MAIL_HOST: "smtp.gmail.com",
+	MAIL_PORT: 465,
+	MAIL_SECURE: true,
+	MAIL_USER: "searchsparrow@gmail.com",
+	MAIL_PASSWORD: "ZKZ78S98VK@h1",
+	MAIL_FROM: "PSL <JOBAYER@gmail.com>",
 }
-export const ormConfig = {
-	type: config.db.type,
-	host: config.db.host,
-	port: config.db.port,
-	username: config.db.username,
-	password: config.db.password,
-	database: config.db.database,
-	synchronize: config.db.synchronize,
-	logging: config.db.logging,
+export const ormConfig: any = {
+	type: "postgres",
+	host: "localhost",
+	port: 5432,
+	username: "root",
+	password: "root",
+	database: "ishop",
+	synchronize: true,
+	logging: false,
 	entities: [__dirname + "/app/@modules/**/**/*.entity{.ts,.js}"],
 }
-		// "heroku-postbuild": "cd back-end && npm install && npm start"
+// "heroku-postbuild": "cd back-end && npm install && npm start"
