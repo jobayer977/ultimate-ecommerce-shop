c6af6a6eb5b649c85a07c70033c610eb91ce7569
Jobayer Hossain <Jobayerhossain977@gmail.com>
2021-06-02 03:37:45 +0600

    ini

--- back-end/src/app/@enums/userType.enum.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.UserType = void 0;
-var UserType;
-(function (UserType) {
-    UserType["ADMIN"] = "ADMIN";
-    UserType["CUSTOMER"] = "CUSTOMER";
-})(UserType = exports.UserType || (exports.UserType = {}));
