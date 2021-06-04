aac78bf059618a320ee9adb9c7b6d7dd6e6e14e1
Jobayer Hossain <Jobayerhossain977@gmail.com>
2021-06-05 00:18:59 +0600

    master: Updated

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -26,7 +26,21 @@ useContainer(Container)
 config()
 //*  Database Connection
 const connectDB = async () => {
-	await createConnection(ormConfig)
+	const connectDB = async () => {
+		if (process.env.DBATABASE_URL) {
+			await createConnection({
+				type: "postgres",
+				url: "postgres://mvqlsmbjohzuys:5d369bee6d76f3193a990a98ba830ca52d5fbc295953407cb221ab90d2498eb0@ec2-34-193-113-223.compute-1.amazonaws.com:5432/dd0j05ms4ol6dm",
+				ssl: {
+					rejectUnauthorized: false,
+				},
+				logging: true,
+				entities: ormConfig.entities,
+			})
+		} else {
+			await createConnection(ormConfig)
+		}
+	}
 }
 
 //* Auth Role Verify
