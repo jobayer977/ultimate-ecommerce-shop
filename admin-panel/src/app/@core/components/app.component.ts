89869e8f4e9be7fb3a2d24981b87fa6144592bf0
Jobayer Hossain <Jobayerhossain977@gmail.com>
2021-06-19 23:56:19 +0600

    master: Updated

--- admin-panel/src/app/@core/components/app.component.ts
+++ admin-panel/src/app/@core/components/app.component.ts
@@ -1,8 +1,22 @@
+import { NavigationEnd, Router } from '@angular/router';
+
 import { Component } from '@angular/core';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(public router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        gtag('config', 'G-272SRQLDVL', {
+          page_path: event.urlAfterRedirects,
+        });
+      }
+    });
+  }
+}
