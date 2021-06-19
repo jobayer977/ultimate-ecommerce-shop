399bc3e0bb86c2ebdabd7bad1ef3da0d15686baf
Jobayer Hossain <Jobayerhossain977@gmail.com>
2021-06-19 16:42:43 +0600

    master: Updated

--- web-panel/src/@modules/user/components/orders.component.tsx
+++ web-panel/src/@modules/user/components/orders.component.tsx
@@ -1,17 +1,27 @@
-import React, { useEffect } from "react"
+import { Button, Table } from "antd"
+import React, { useEffect, useState } from "react"
 
 import { BaseFilterResponse } from "@shared/interfaces/base.interface"
 import { OrderService } from "@shared/services/order.service"
-import { Table } from "antd"
 import { getSession } from "@shared/utils/jwttoken"
+import moment from "moment"
+import { takaCurrencySing } from "@shared/constant/preferance"
 import useService from "@shared/hooks/useService"
 
 const OrdersComponent = () => {
 	const user: any = getSession()
+	const [ordersResponse, setOrdersResponse] = useState<BaseFilterResponse>({
+		data: [],
+		page: 0,
+		take: 0,
+		total: 0,
+	})
+	const { data, page, take, total } = ordersResponse
 	const filterOrderService = useService(
 		OrderService.filter,
 		(res: BaseFilterResponse) => {
 			console.log(res)
+			setOrdersResponse(res)
 		}
 	)
 
@@ -22,18 +32,44 @@ const OrdersComponent = () => {
 			user: user?.id,
 		})
 	}, [user?.id])
-	const dataSource = [
-		{
-			orderCode: "orderCode",
-			date: "Order Code",
-			total: 32,
-			status: "10 Downing Street",
-		},
-	]
+	const dataSource = ordersResponse?.data?.map((o: any) => ({
+		orderCode: o?.code,
+		date: moment(o?.createdAt).format("DD-MM-YYY HH:MM A"),
+		total: `${takaCurrencySing}${o?.totalAmount} TK`,
+		status: (
+			<Button type="primary" danger={!o.approved}>
+				{o.deliveryStatus}
+			</Button>
+		),
+	}))
 
 	return (
 		<>
-			<Table dataSource={dataSource} columns={columns} />
+			{/* <Table
+				loading={filterOrderService.loading}
+				dataSource={dataSource}
+				columns={columns}
+			/> */}
+
+			<Table
+				loading={filterOrderService.loading}
+				columns={columns}
+				dataSource={dataSource}
+				scroll={{ y: 640 }}
+				pagination={{
+					onChange: (e: any) => {
+						filterOrderService.query({
+							user: user?.id,
+							page: e,
+							take: 10,
+						})
+					},
+
+					pageSize: 10,
+					total: total,
+					showSizeChanger: false,
+				}}
+			/>
 		</>
 	)
 }
