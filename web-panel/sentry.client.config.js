56f641d2f42242eede38199471d6b03215dab779
Jobayer Hossain <Jobayerhossain977@gmail.com>
2021-06-20 00:38:53 +0600

    master: Added sentry Issue Tracker

---
+++ web-panel/sentry.client.config.js
@@ -0,0 +1,14 @@
+// This file configures the initialization of Sentry on the browser.
+// The config you add here will be used whenever a page is visited.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+
+Sentry.init({
+  dsn: SENTRY_DSN || 'https://85a433d1612847afa11956f1f5c826e6@o871183.ingest.sentry.io/5824654',
+  // Note: if you want to override the automatic release value, do not set a
+  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+  // that it will also get attached to your source maps
+});
